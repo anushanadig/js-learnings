3012b1921271f68b8983d704ee9007ec45398e8f
Anusha <anushanadigm@gmail.com>
2019-01-15 19:40:56 +0530

    learning generators and iterators

---
+++ generators.js
@@ -0,0 +1,6 @@
+// a generator appears to be a function but it behaves like an iterator.
+// Generators are a special class of functions that simplify the task of writing iterators.
+// A generator is a function that produces a sequence of results instead of a single value,
+//  i.e you generate ​a series of values.
+
+//  Remember that every function implicitly returns undefined if no return statement is provideds
---
+++ iterators.js
@@ -0,0 +1,6 @@
+// An iterable is a data structure that wants to make its elements accessible to the public.
+// It does so by implementing a method whose key is Symbol.iterator. That method is a factory for iterators.
+// That is, it will create iterators.
+// An iterator is a pointer for traversing the elements of a data structure.
+
+//Computed Property Names
